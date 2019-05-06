5a41e17a0e347c16f67bfb5c6cc44c80c50523d0
Stéphane Brunner <stephane.brunner@camptocamp.com>
2019-05-06 18:00:45 +0200

    fix

--- travis/doc.sh
+++ travis/doc.sh
@@ -2,41 +2,19 @@
 
 ./docker-run make -f travis.mk doc
 
-DOC=false
-BRANCH=2.1
-
-if [[ ${TRAVIS_BRANCH} =~ ^(master|[0-9]+.[0-9]+)$ ]] && [ ${TRAVIS_PULL_REQUEST} == false ]
-then
-    DOC=true
-    BRANCH=${TRAVIS_BRANCH}
-fi
-
+BRANCH=2.2
 
 echo == Build the doc ==
 
 git fetch origin gh-pages:gh-pages
 git checkout gh-pages
 
-if [ -e ${BRANCH} ]
-then
-    git rm -r --force -- ${BRANCH}
-fi
 mkdir --parent ${BRANCH}
-mv doc/_build/html/* ${BRANCH}
+mv doc/_build/html/* ${BRANCH}|true
 
-if [ ${DOC} == true ]
-then
-    git add --all ${BRANCH}
-    git commit --message="Update documentation for the revision ${TRAVIS_COMMIT}" | true
-    git push origin gh-pages
-else
-    git checkout ${BRANCH}/searchindex.js || true
-    git checkout ${BRANCH}/_sources || true
-    git checkout ${BRANCH}/_static || true
-    git status
-    git diff
-    git reset --hard
-fi
+git add --all ${BRANCH}
+git commit --amend --message="Update documentation for the revision ${TRAVIS_COMMIT}" | true
+git push --force origin gh-pages
 
 # back to the original branch
 git checkout ${GIT_REV}
